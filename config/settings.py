7f59de5ad4315677c5ffb5f803554a7b7dd2cbd5
Ronuel Joshua B. Reyes <ronuelreyes@gmail.com>
2023-07-22 14:44:20 +0800

    22/07/2023 Settings 1. Fixed Corsheaders error

--- config/settings.py
+++ config/settings.py
@@ -29,6 +29,11 @@ ALLOWED_HOSTS = []
 
 
 # Application definition
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:8000/",
+    "http://127.0.0.1:9000/",
+]
+CORS_ORIGIN_ALLOW_ALL = True
 
 INSTALLED_APPS = [
     'django.contrib.admin',
